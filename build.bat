41d2b1292abca4b6c25b8f3ffece379a1d048a0f
TPPPP <90049492+TTTPPPPP@users.noreply.github.com>
2023-08-22 18:24:05 +0800

    add -Wall option

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-g++ -static encrypt.cpp PFile.cpp base64.hpp -o encrypt.exe
+g++ -Wall -static encrypt.cpp PFile.cpp base64.hpp -o encrypt.exe
 strip encrypt.exe
 echo.finished!
 pause>nul
